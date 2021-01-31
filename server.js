bb913a6f7911737f08c8f2472e0a79d8de7dc368
Puentebravo <puentematos@gmail.com>
2021-01-31 17:35:39 -0500

    added app passthroughs to routing files

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var PORT = process.env.PORT || 3001
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("../routes/apiRoutes")(app);
-require("../routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function() {
     console.log(`Server online. Now listening on port ${PORT}`)
