e2ec34bfe84d6e946fb63501384b207e98618179
Puentebravo <puentematos@gmail.com>
2021-01-31 17:35:02 -0500

    added server skeleton to server.js, moved into root dir

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+var app = express();
+
+var PORT = process.env.PORT || 3001
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("../routes/apiRoutes")(app);
+require("../routes/htmlRoutes")(app);
+
+app.listen(PORT, function() {
+    console.log(`Server online. Now listening on port ${PORT}`)
+});
