b801cc43cd7eaa52a73c292d3ef71d096459f530
Puentebravo <puentematos@gmail.com>
2021-02-01 18:25:55 -0500

    Wrote GET API route, fixed import statements

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,13 @@
-import path from "path";
-import db from "../db/db.json"
+const path = require ("path")
+const fs = require ("fs");
 
 module.exports = function (app) {
-    app.get("/api/db", function(req, res) {
-        res.json.parse(db);
-        console.log(db)
+    app.get("/api/notes", function(req, res) {
+        fs.readFile(path.join(__dirname, "../db/db.json"), function (err, data) {
+            if (err) throw (err);
+            res.json(JSON.parse(data))
+        })
+        
     });
     // app.post("/api/db", function(req, res) {
 
