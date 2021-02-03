95a1065494cc0b14d39f93248b5d815b2a6e73e8
Puentebravo <puentematos@gmail.com>
2021-02-03 00:42:17 -0500

    Wrote loop to add unique ID to each JSON entry

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,11 +8,19 @@ module.exports = function (app) {
     });
     
     app.post("/api/notes", function(req, res) {
-        notes.push(req.body);
+        const note = req.body;
+        notes.push(note);
+        for (let i = 0 ; i < notes.length; i++) {
+            notes[i].id = i + 1
+        };
         fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(notes), (err) => {
             if (err) throw err;
            res.json(notes);
-        })
+        });
         
-    })
+    });
+
+    // app.delete("/api/notes/:id", function(req, res) {
+
+    // })
 };
